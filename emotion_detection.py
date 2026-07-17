22ed7ce057aaa8a91b84c963bc1ad7ff3264d825
jdgedv <88644745+jdgedv@users.noreply.github.com>
2026-07-17 14:18:04 -0500

    Refactor emotion_detector to include emotion scores
    Enhance emotion_detector function to return detailed emotion scores and dominant emotion.

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,3 +1,4 @@
+import json
 import requests
 
 
@@ -7,4 +8,29 @@ def emotion_detector(text_to_analyze):
     input_json = {"raw_document": {"text": text_to_analyze}}
 
     response = requests.post(url, json=input_json, headers=headers)
-    return response.text
+    response_dict = json.loads(response.text)
+    emotions = response_dict['emotionPredictions'][0]['emotion']
+
+    anger_score = emotions['anger']
+    disgust_score = emotions['disgust']
+    fear_score = emotions['fear']
+    joy_score = emotions['joy']
+    sadness_score = emotions['sadness']
+
+    scores = {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+    }
+    dominant_emotion = max(scores, key=scores.get)
+
+    return {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion,
+    }
