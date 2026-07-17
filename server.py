f1aab222c7314df602044567ca914ae4c73bbf20
jdgedv <88644745+jdgedv@users.noreply.github.com>
2026-07-17 15:02:54 -0500

    Validate dominant emotion before processing response
    Added validation for dominant emotion in emot_detector function.

--- server.py
+++ server.py
@@ -9,12 +9,16 @@ def emot_detector():
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
 
+    dominant_emotion = response['dominant_emotion']
+
+    if dominant_emotion is None:
+        return "Invalid text! Please try again!"
+
     anger = response['anger']
     disgust = response['disgust']
     fear = response['fear']
     joy = response['joy']
     sadness = response['sadness']
-    dominant_emotion = response['dominant_emotion']
 
     return (f"For the given statement, the system response is 'anger': {anger}, "
             f"'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. "
